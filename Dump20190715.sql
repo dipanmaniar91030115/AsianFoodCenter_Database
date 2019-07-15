224d2164ad349d8d918b471d5bef21993f1782f1
dipanmaniar91030115 <46704312+dipanmaniar91030115@users.noreply.github.com>
2019-07-15 17:37:55 +1200

    Database Iteration 1
    This is a database which is to be used for a mobile and web application in the future, the team members would be adding the access rights and the data soon

---
+++ Dump20190715.sql
@@ -0,0 +1,147 @@
+-- MySQL dump 10.13  Distrib 8.0.16, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: asianfoodcenter
+-- ------------------------------------------------------
+-- Server version	8.0.16
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `arts`
+--
+
+DROP TABLE IF EXISTS `arts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `arts` (
+  `CatID` int(2) DEFAULT NULL,
+  `Title` varchar(30) DEFAULT NULL,
+  `BirthYear` varchar(4) DEFAULT NULL,
+  `DeathYear` varchar(4) DEFAULT NULL,
+  `Nationality` varchar(30) DEFAULT NULL,
+  `KnownFor` varchar(30) DEFAULT NULL,
+  `Notablework` varchar(100) DEFAULT NULL,
+  `About` varchar(1000) DEFAULT NULL,
+  `TimeOfCreation` varchar(11) DEFAULT NULL,
+  `MediumDetails` varchar(30) DEFAULT NULL,
+  `Dimensions` varchar(7) DEFAULT NULL,
+  `Location` varchar(30) DEFAULT NULL,
+  KEY `CatID_idx` (`CatID`),
+  CONSTRAINT `CatID` FOREIGN KEY (`CatID`) REFERENCES `category` (`CatID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `arts`
+--
+
+LOCK TABLES `arts` WRITE;
+/*!40000 ALTER TABLE `arts` DISABLE KEYS */;
+INSERT INTO `arts` VALUES (1,'Leonardo da Vinci','1452','1519','Italian','Art and Science','Mona Lisa, The Last Supper','Leonardo di ser Piero da Vinci, more commonly Leonardo da Vinci, was an Italian polymath of the Renaissance, whose areas of interest included invention, painting, sculpting, architecture, science, music, mathematics, engineering, literature, anatomy, geology, astronomy, botany, writing, history, and cartography. He has been variously called the father of palaeontology, ichnology, and architecture, and is widely considered one of the greatest painters of all time. Sometimes credited with the inventions of the parachute, helicopter and tank, he epitomised the Renaissance humanist ideal.','','','','');
+/*!40000 ALTER TABLE `arts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `category` (
+  `CatID` int(2) NOT NULL,
+  `CategoryName` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`CatID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `category`
+--
+
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+INSERT INTO `category` VALUES (1,'Biography');
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `maths`
+--
+
+DROP TABLE IF EXISTS `maths`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `maths` (
+  `CatID` int(2) DEFAULT NULL,
+  `Title` varchar(30) DEFAULT NULL,
+  `CreatorKnownFor` varchar(30) DEFAULT NULL,
+  `BirthYear` varchar(4) DEFAULT NULL,
+  `DeathYear` varchar(4) DEFAULT NULL,
+  `About` varchar(1000) DEFAULT NULL,
+  KEY `CatID_idx` (`CatID`),
+  CONSTRAINT `maths_ibfk_1` FOREIGN KEY (`CatID`) REFERENCES `category` (`CatID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `maths`
+--
+
+LOCK TABLES `maths` WRITE;
+/*!40000 ALTER TABLE `maths` DISABLE KEYS */;
+INSERT INTO `maths` VALUES (1,'Euclid','Euclidean geometry','4BC','3BC','Euclid, sometimes given the name Euclid of Alexandria to distinguish him from Euclides of Megara, was a Greek mathematician, often referred to as the \"founder of geometry\" or the \"father of geometry\". He was active in Alexandria during the reign of Ptolemy I (323–283 BC). His Elements is one of the most influential works in the history of mathematics, serving as the main textbook for teaching mathematics (especially geometry) from the time of its publication until the late 19th or early 20th century. In the Elements, Euclid deduced the theorems of what is now called Euclidean geometry from a small set of axioms. Euclid also wrote works on perspective, conic sections, spherical geometry, number theory, and rigor.');
+/*!40000 ALTER TABLE `maths` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `technology`
+--
+
+DROP TABLE IF EXISTS `technology`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `technology` (
+  `CatID` int(2) DEFAULT NULL,
+  `Title` varchar(30) DEFAULT NULL,
+  `BirthYear` varchar(4) DEFAULT NULL,
+  `DeathYear` varchar(4) DEFAULT NULL,
+  `Nationality` varchar(30) DEFAULT NULL,
+  `KnownFor` varchar(50) DEFAULT NULL,
+  `About` varchar(1000) DEFAULT NULL,
+  `DesignedBy` varchar(30) DEFAULT NULL,
+  `DeveloperName` varchar(30) DEFAULT NULL,
+  KEY `CatID_idx` (`CatID`),
+  CONSTRAINT `technology_ibfk_1` FOREIGN KEY (`CatID`) REFERENCES `category` (`CatID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `technology`
+--
+
+LOCK TABLES `technology` WRITE;
+/*!40000 ALTER TABLE `technology` DISABLE KEYS */;
+INSERT INTO `technology` VALUES (1,'Steve Jobs','1955','2011','USA','Co-creator of the Macintosh & Apple','Steven Paul Jobs was an American entrepreneur and business magnate. He was the chairman, chief executive officer (CEO), and a co-founder of Apple Inc., chairman and majority shareholder of Pixar, a member of The Walt Disney Companys board of directors following its acquisition of Pixar, and the founder, chairman, and CEO of NeXT. Jobs is widely recognized as a pioneer of the microcomputer revolution of the 1970s and 1980s, along with Apple co-founder Steve Wozniak','','');
+/*!40000 ALTER TABLE `technology` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-07-15 17:12:05
